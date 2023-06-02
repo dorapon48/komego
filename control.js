2cdb86fcbe2e8fca701ee3cc166bc9decea2b8ba
dorapon48 <dorapon48@icloud.com>
2023-06-02 17:34:09 +0900

    add: コメ君へのメッセージ送信関数, fix: nameをidに変更

--- control.js
+++ control.js
@@ -1,1 +1,14 @@
 
+const send_kome_id = "chat-kome";
+const send_you_id = "chat-you";
+const chat_outputs_id = "chat-outputs";
+
+function send_kome(){
+    let element = document.getElementById(send_kome_id);
+    let text = element.value;
+    if (text != ""){
+
+    }
+    element.value = "";
+    return 0;
+}
