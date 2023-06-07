a3d03dec6dbc9ca9efa456dbcf0d24d18c8639ad
dorapon48 <dorapon48@icloud.com>
2023-06-07 18:29:59 +0900

    add: あなたに対するチャットを追加できるように関数を追加

--- src/components/chat.js
+++ src/components/chat.js
@@ -4,31 +4,31 @@ import './chat.css';
 import ChatOutputYou from './chat_you';
 import ChatOutputKome from './chat_kome';
 
-function test2(test){
-    if (test == 'テスト'){
+function AddChat(obj){
+    if (obj.who){
         return (
-            <p>testtest</p>
+            <ChatOutputYou text={obj.chat} />
         )
     }
     return (
-        <p>test</p>
+        <ChatOutputKome text={obj.chat} key={obj.chat}/>
     )
-    
 }
+
 
 function Chat() {
     const you_chat_text = useRef(null);
     const kome_chat_text = useRef(null);
-    const test = [
-        {
-            chat: 'テスト'
-        },
-        {
-            chat: 'テスト2'
-        },
-    ]
     const [chats, setChats] = useState([]);
-    
+
+    function ClickSubmitYou(text){
+        console.log(text);
+        const t = {
+            chat: text,
+            who: false
+        }
+        setChats([...chats, t])
+    }
 
     return (
         <div className="chat">
@@ -36,14 +36,14 @@ function Chat() {
                 <ChatOutputYou text='テストメッセージ'></ChatOutputYou>
                 <ChatOutputKome text='テストメッセージ'></ChatOutputKome>
                 { chats.map((chat) => (
-                    test2(chat.chat)
+                    AddChat(chat)
                 ))}
             </div>
             <div className="chat-inputs">
                 <div className="chat-you">
                     <label>あなたへ:</label>
                     <input type="text" ref={you_chat_text}/>
-                    <input type="submit" value="送信"/>
+                    <input type="submit" value="送信" onClick={() => ClickSubmitYou(you_chat_text.current.value)}/>
                 </div>
                 <div className="chat-kome">
                     <label>コメ君へ:</label>
