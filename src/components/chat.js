675bf5d3e58edef6be9eec89accc450df33f5bec
dorapon48 <dorapon48@icloud.com>
2023-06-07 18:42:52 +0900

    add: コメ君への送信ボタンが押されたときの処理を追加, コメント追加

--- src/components/chat.js
+++ src/components/chat.js
@@ -4,14 +4,21 @@ import './chat.css';
 import ChatOutputYou from './chat_you';
 import ChatOutputKome from './chat_kome';
 
+/**
+ * チャットに内容をreactElementで返す
+ * obj.whoでコメ君かどうかを判定し，それぞれ返す
+ * @param {Object} obj {chat: {String}, who: {Boolen}}
+ * @returns {import('react').ReactElement}
+ */
 function AddChat(obj){
+    // コメ君なら右側に表示される
     if (obj.who){
         return (
-            <ChatOutputYou text={obj.chat} />
+            <ChatOutputYou text={obj.chat} key={Math.random()}/>
         )
     }
     return (
-        <ChatOutputKome text={obj.chat} key={obj.chat}/>
+        <ChatOutputKome text={obj.chat} key={Math.random()}/>
     )
 }
 
@@ -21,13 +28,28 @@ function Chat() {
     const kome_chat_text = useRef(null);
     const [chats, setChats] = useState([]);
 
+    /**
+     * あなたへの送信ボタンが押されたときの処理
+     * @param {String} text 
+     */
     function ClickSubmitYou(text){
-        console.log(text);
         const t = {
             chat: text,
             who: false
-        }
-        setChats([...chats, t])
+        };
+        setChats([...chats, t]);
+    }
+
+    /**
+     * コメ君への送信ボタンが押されたときの処理
+     * @param {String} text 
+     */
+    function ClickSubmitKome(text){
+        let t = {
+            chat: text,
+            who: true
+        };
+        setChats([...chats, t]);
     }
 
     return (
